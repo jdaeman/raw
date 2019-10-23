2d720d9c5d3166d030f8cc51cb26e7d876cbde22
jdaeman <xordn25@naver.com>
2019-10-23 13:07:36 +0900

    raw.c

--- raw.c
+++ raw.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(void)
+#define BUF_SIZE 65536
+
+int main(int argc, char ** argv[])
 {
-	printf("Hello world...\n");
+	int sniff_sock;
+	unsigned char buf[BUF_SIZE];
+	
+	printf("%s\n", argv[0]);
+		
+
+	//argv[0] :
+	//argv[1] :
+	//argv[2] :
+	//argv[3] :
+
 	return 0;
 }
