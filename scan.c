b5bb5a15ecf9c0380549b5ae210d4d803ef28269
jdaeman <xordn25@naver.com>
2019-12-01 19:15:30 +0900

    portscan

--- scan.c
+++ scan.c
@@ -5,56 +5,30 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <linux/tcp.h>
+#include <linux/ip.h>
 #include "util.c"
 
-/*for (port= 443; port <= 65536; port++)
-	{
-		sock = socket(PF_INET, SOCK_STREAM, 0);
-		if (sock < 0)
-		{
-			perror("socket() error");
-			break;
-		}
-
-		if (setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0)
-		{
-			perror("setsockopt() error");
-			break;
-		}
-		if (setsockopt(sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0)
-		{
-			perror("ttl() error");
-			break;
-		}
-
-		addr.sin_port = htons(port);
-	
-		if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-			perror("connect() error");
-		else
-			printf("port[%u] is opend\n", port);
-		
-		close(sock);
-	}
-*/
+struct pseudo
+{
+	unsigned int src;
+	unsigned int dst;
+	unsigned char resv;
+	unsigned char proto;
+	unsigned short tcpseg_len;
+};
 
 int main(int argc, char ** argv)
 {
 	int sock;
-	struct sockaddr_in addr, raddr;
-	unsigned short port = 1;
-	int cnt, ttl = 1;
-	struct timeval timeout;
-	unsigned char pkt[1024], rep[1024];
+	struct sockaddr_in addr;
+	unsigned short port = 80;
+	unsigned char pkt[1024], * tt;
+	unsigned char pseudo[12];
 	struct tcphdr * tcp;
-	int len, ret;
-	int on = 0;
-
-	struct sockaddr_in saddr;
-
-	unsigned char * tt;
-	//if (argc == 1)
-		//return -1;
+	struct iphdr * ip;
+	int on = 1, ret;
+	int proto = IPPROTO_TCP, tcp_len = 20;
+	struct pseudo * pp;
 
 	sock = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
 	if (sock < 0)
@@ -63,70 +37,79 @@ int main(int argc, char ** argv)
 		return -1;
 	}
 
-	if (setsockopt(sock, IPPROTO_IP, IP_NODEFRAG, &on, sizeof(on)) < 0)
-	{
-		perror("setsockopt() error");
-		return -1;
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = PF_INET;
-	addr.sin_addr.s_addr = inet_addr("192.168.0.1");
-	addr.sin_port = htons(80);
-
-	/*if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-{
-	perror("connect()");
-	return -1;
-}
-else
-	return 0;*/
-	
-	/*timeout.tv_sec = 3;
-	timeout.tv_usec = 0;
-	if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0)
+	/*if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0)
 	{
 		perror("setsockopt() error");
 		return -1;
 	}*/
 
-	for (port= 80; port <= 65536; port++)
-	{
-		memset(pkt, 0, sizeof(pkt));
-		//addr.sin_port = htons(port);
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = PF_INET;
+	addr.sin_addr.s_addr = inet_addr("192.168.0.1");
+	addr.sin_port = htons(port);
+
+	memset(pkt, 0, sizeof(pkt));
 	
-		tcp = (struct tcphdr *)pkt;
-		tcp->source = htons(51234);
-		tcp->dest = htons(port);
-		tcp->seq = htonl(123456);
-		tcp->doff = 6;
-		tcp->syn = 1;			
-		tcp->window = htons(1024);
+	tcp = (struct tcphdr *)(pkt + 12);
+	tcp->source = htons(51234);
+	tcp->dest = htons(port);
+	tcp->seq = htonl(172147);
+	tcp->ack_seq = 0;
+	tcp->doff = 5;
+	tcp->syn = 1;			
+	tcp->window = htons(1024);
+	//1. pseudo-header
+	//|- src-addr(4) + dst-addr(4) + reserved(1) + protocol(1) + tcp-length(2)
+	//2. tcp-header
+	//3. tcp-payload
+	pp = (struct pseudo *)pkt;
+	pp->src = inet_addr("192.168.0.7");
+	pp->dst = inet_addr("192.168.0.1");
+	pp->proto = IPPROTO_TCP;
+	pp->tcpseg_len = htons(tcp->doff << 2);
 
-		tt = (unsigned char *)(tcp + 1);
-		tt[0] = 2;
-		tt[1] = 4;
-		tt[2] = 0x05;
-		tt[3] = 0xb4;
-		tcp->check = cksum(pkt, sizeof(struct tcphdr) + 4);
+	tcp->check = cksum(pkt, 12 + 20);
 
-		if (sendto(sock, pkt, sizeof(struct tcphdr) + 4, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-		{
-			perror("sendto() error");
-			break;
-		}
+	/*ip = (struct iphdr *)pkt;
+	ip->ihl = 5;
+	ip->version = 4;	
+	ip->tos = 0;
+	ip->tot_len = 40;
+	ip->id = htons(21423);
+	ip->frag_off = 0;
+	ip->ttl = 64;
+	ip->protocol = IPPROTO_TCP;
+	ip->saddr = inet_addr("192.168.0.7");
+	ip->daddr = inet_addr("192.168.0.1");
+	
+	memset(&pp, 0, sizeof(pp));
+	pp.src = ip->saddr;
+	pp.dst = ip->daddr;
+	pp.proto = IPPROTO_TCP;
+	pp.tcpseg_len = tcp->doff << 2;
+	memcpy(pp.ptr, tcp, tcp->doff << 2);
+ 
+	tcp->check = cksum((unsigned char *)&pp , 12 + 20);
+	ip->check = cksum(pkt, 40);*/
 
-		ret = recvfrom(sock, rep, sizeof(rep), 0, NULL, NULL); 
-		if (ret < 0)
-		{
-			perror("recvfrom() error");
-			continue;
-		}
-		
-		rep[ret] = 0;
-		printf("len, %d, %u is open\n", ret, port);	
+	if (sendto(sock, pkt + 12, 20, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
+		perror("sendto() error");
+		return -1;
 	}
+
+	ret = recvfrom(sock, pkt, sizeof(pkt), 0, NULL, NULL); 
+	if (ret < 0)
+	{
+		perror("recvfrom() error");
+		return -1;
+	}
+	else
+		printf("%d received\n", ret);
 
 	close(sock);
 	return 0;
 }
+
+// https://stackoverflow.com/questions/29877735/not-receiving-syn-ack-after-sending-syn-using-raw-socket
+
